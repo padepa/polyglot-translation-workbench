09033c62a63b345e6112b36874bd42f302eaa3c9
padepa <39232196+padepa@users.noreply.github.com>
2026-05-19 19:54:10 +0800

    Update start_workbench.bat for polyglot naming

--- start_workbench.bat
+++ start_workbench.bat
@@ -1,8 +1,8 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
-if exist "dist\TripleTranslateWorkbench\TripleTranslateWorkbench.exe" (
-    start "" "%~dp0dist\TripleTranslateWorkbench\TripleTranslateWorkbench.exe"
+if exist "dist\PolyglotTranslationWorkbench\PolyglotTranslationWorkbench.exe" (
+    start "" "%~dp0dist\PolyglotTranslationWorkbench\PolyglotTranslationWorkbench.exe"
 ) else (
     start "" pythonw native_tk_app.py
 )
